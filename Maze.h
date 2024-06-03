59d70ea71f02584541ae8e63b6ce973c32ff0571
maxwell glover <86388051+maxglover@users.noreply.github.com>
2024-06-03 01:16:22 -0500

    Update Maze.h

---
+++ Maze.h
@@ -0,0 +1,54 @@
+// Class to generate a random maze using disjoint sets and print it in ASCII format to a file
+// By max glover
+//it 279
+//maze header
+
+#ifndef MAZE_H
+#define MAZE_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct CellWalls
+{
+    bool south;
+    bool east;
+    CellWalls(bool startEast = true, bool startSouth = true) : east(startEast), south(startSouth) {}
+};
+
+class Maze
+{
+private:
+    CellWalls *mazeWalls; // maze cells
+    int numRows;
+    int numColumns;
+
+public:
+    // constructor
+    Maze(int rows, int cols);
+
+    // copy constructor
+    Maze(const Maze &orig) { copy(orig); };
+
+    // destructor
+    ~Maze() { delete [] mazeWalls; }
+
+    // assignment operator
+    Maze &operator=(const Maze &rhs);
+
+    // generates the maze by randomly knocking down walls
+    void generateMaze();
+
+    //generates maze that stops early
+    void generateEarlyMaze();
+
+    // prints the maze to the specified outputStream
+    void print(ostream &outputStream);
+
+private:
+    // private helper method to copy the data from another Maze object
+    void copy(const Maze &orig);
+};
+
+#endif
